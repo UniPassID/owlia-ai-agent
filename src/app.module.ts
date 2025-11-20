6480b1f993342eaddd1e5e394e29299830be6a47
zz <48124374+zzhengzhuo@users.noreply.github.com>
2025-11-20 18:52:26 +0800

    feat: update config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [AccountModule],
+  imports: [
+    AccountModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

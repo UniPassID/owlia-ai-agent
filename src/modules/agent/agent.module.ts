a57275cd5b0adee411073d64adaf656e09efe35d
zz <48124374+zzhengzhuo@users.noreply.github.com>
2025-12-05 16:07:55 +0800

    feat: update modules

--- src/modules/agent/agent.module.ts
+++ src/modules/agent/agent.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AgentController } from './agent.controller';
 import { DocService } from './docs.service';
+import { AgentService } from './agent.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AgentController],
-  providers: [DocService],
-  exports: [DocService],
+  providers: [DocService, AgentService],
+  exports: [DocService, AgentService],
+  imports: [ConfigModule],
 })
 export class AgentModule {}

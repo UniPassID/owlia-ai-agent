080e8f60c3cdff7c49920137e5083befdee6d100
johnz <johnz@lay2.dev>
2025-12-11 15:30:24 +0800

    fix: lp in range condition

--- src/monitor/rebalance-precheck.service.ts
+++ src/monitor/rebalance-precheck.service.ts
@@ -358,7 +358,7 @@ export class RebalancePrecheckService {
                 tickLower !== null &&
                 tickUpper !== null
               ) {
-                if (currentTick >= tickLower && currentTick <= tickUpper) {
+                if (currentTick >= tickLower && currentTick < tickUpper) {
                   hasInRangeLp = true;
                   break;
                 }
